cfd840c8a18d5a1c58e24efc30f106e4153d718d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-31 13:40:49 +0530

    imported and wired funnctions

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,71 +1,24 @@
 import express from 'express'
-import BlogModel from "./../models/blogModel.js";
+import { addBlog,deleteBlog,getAllBlogs,getSingleBlog,redirectAboutPage,renderAboutPage,renderContactPage,renderCreatePage } from './../controllers/blogController.js'
 
 const Router = express.Router()
 
 
-Router.get('/',(req,res)=>{
-    try {
-        BlogModel.find().sort({ createdAt: -1 })
-        .then(blogs => res.render('home',{ title: 'Home' , blogs}))
-        .catch(err => console.log(err))
-    } catch (error) {
-        console.log("Some error :",error)
-    }
-    // res.sendFile('/views/home.html', { root: __dirname })
-})
+Router.get('/',getAllBlogs)
 
-Router.get('/create',(req,res)=>{
-    res.render('create',{ title: 'Create' })
-})
+Router.get('/create',renderCreatePage)
 
-Router.get('/about',(req,res)=>{
-    // res.sendFile('/views/about.html', { root: __dirname })
-    res.render('about',{ title: 'About' })
-})
+Router.get('/about',renderAboutPage)
 
-Router.get('/contact',(req,res)=>{
-    res.render('contact',{ title: 'Contact' })
-})
+Router.get('/contact',renderContactPage)
 
-Router.get('/aboutme',(req,res)=>{
-    res.redirect('/about')
-})
+Router.get('/aboutme',redirectAboutPage)
 
-Router.post('/addblog',(req,res) => {
-    const blog = new BlogModel(req.body)
-    blog.save()
-        .then((result)=>{
-            console.log(result)
-            res.json({redirectUrl: '/'})
-        })
-        .catch((err)=>{
-            console.log("Some error",err);
-        })
-})
+Router.post('/addblog',addBlog)
 
-Router.get('/blogs/:id',async(req,res) => {
-    const { id } = req.params;
+Router.get('/blogs/:id',getSingleBlog)
 
-    try {
-        const blog = await BlogModel.findById(id)
-        const updateResult = await BlogModel.findByIdAndUpdate(id,{ $inc: { readBy: 1 } })
-        res.render('details', { blog, title: blog.blogTitle })
-    } catch (error) {
-        console.log("Some error :",error)
-    }
-})
-
-Router.delete('/delBlog/:id',async(req,res) => {
-    const { id } = req.params;
-
-    try {
-        await BlogModel.findByIdAndDelete(id);
-        res.json({redirectUrl: '/'})
-    } catch (error) {
-        console.log("Some error :",error)
-    }
-})
+Router.delete('/delBlog/:id',deleteBlog)
 
 
 export default Router;
